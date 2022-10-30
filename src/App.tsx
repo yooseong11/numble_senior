a07b41d70bf8a15a38f304277b57679e2d4db157
유성 <82145837+yuseong11@users.noreply.github.com>
2022-10-30 09:47:19 +0900

    feat: 기본 색상, 폰트, 레이아웃 설정 (#3)
    * feat: 색상 설정
    
    * feat: font설정
    
    * layout 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css';
+import header from './image/header.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <main></main>
+    <div className='App bg-gray-400 h-screen'>
+      <div className='flex justify-center items-center'>
+        <div id='screen' className=' bg-white h-screen w-[22.5rem] px-4'>
+          <header>
+            <img src={header} alt='header' />
+          </header>
+          layout
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
