4fd70c0a0b1f0dd113e61d122eb6d0a69ec92e19
유성 <82145837+yuseong11@users.noreply.github.com>
2022-10-30 10:00:41 +0900

    feat: 라우팅 (#4)
    * feat: react-router-dom 설치
    
    * feat: 라우팅 경로 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const App = React.lazy(() => import('./App'))
+const Onboarding = React.lazy(() => import('./routes/onboarding'))
+const Splash = React.lazy(() => import('./routes/splash'))
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/onboarding',
+        element: <Onboarding />,
+      },
+      {
+        path: '/splash',
+        element: <Splash />,
+      },
+    ],
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
 
-reportWebVitals();
+reportWebVitals()
---
+++ src/routes/splash.tsx
@@ -0,0 +1,12 @@
+const splash = () => {
+  return (
+    <div className='flex flex-col'>
+      <p>
+        당신의 <span className='text-yellow-500'>인생 2막</span>을 응원하는
+      </p>
+      <p>평생 금융파트너</p>
+    </div>
+  )
+}
+
+export default splash
