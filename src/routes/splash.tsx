ed6a258dfd6dac6b2e76655e8ab61a84655aa7e3
유성 <82145837+yuseong11@users.noreply.github.com>
2022-10-30 20:10:15 +0900

    feat: splash 페이지 완성(#5)
    * fix: 폰트가 적용되지 않는 오류
    
    * feat: 스플래쉬 이미지 완성

--- src/routes/splash.tsx
+++ src/routes/splash.tsx
@@ -1,10 +1,17 @@
+import splashImg from '../image/splash.png'
+import kbImg from '../image/kb.png'
+
 const splash = () => {
   return (
-    <div className='flex flex-col'>
-      <p>
-        당신의 <span className='text-yellow-500'>인생 2막</span>을 응원하는
-      </p>
-      <p>평생 금융파트너</p>
+    <div className='flex flex-col items-center font-MICEGothc py-20'>
+      <div className='text-center'>
+        <p className='text-xl'>
+          당신의 <span className='text-yellow-500 font-bold'>인생 2막</span>을 응원하는
+        </p>
+        <p className='text-2xl'>평생 금융파트너</p>
+      </div>
+      <img className='pt-14 w-60' src={splashImg} title='splashimg' />
+      <img className='w-44 pt-14 animate-pulse' src={kbImg} title='splashimg' />
     </div>
   )
 }
