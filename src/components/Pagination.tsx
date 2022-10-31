91ff4cc380fcc00d23c7643913a5517e1a758ff2
LeeYuseong <82145837+yuseong11@users.noreply.github.com>
2022-10-31 16:09:05 +0900

    fix: 다음버튼 "다음" 텍스트 컴포넌트 내부로, Pagination 로직 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,17 +4,22 @@ interface PaginationProps {
 }
 
 const Pagination = ({ size, activeNumber }: PaginationProps) => {
-	const newArray = Array.from({ length: size }, (v, i) => i + 1)
+  const newArray = Array.from({ length: size }, (v, i) => i + 1)
 
-	const active = activeNumber ? 'bg-gray-400 w-4' : 'bg-gray-350 w-2';
-	console.log(newArray)
-	return (
-		<div className='flex'>
-			{newArray.map((item, index) => (
-				(item !== activeNumber)?
-				<div key={index} className='rounded-full h-[0.375rem] m-[0.188rem] bg-gray-350 w-[0.375rem]' />
-				:<div key={index} className='rounded-full h-[0.375rem] m-[0.188rem] bg-gray-400 w-[1.125rem]' />
-			))}
+	const isActiveNumber = (currentPage: number) => currentPage === activeNumber
+	
+  return (
+    <div className='flex'>
+      {newArray.map((item, index) => (
+        <div
+          key={index}
+          className={
+            'rounded-full h-[0.375rem] m-[0.188rem] ' + (isActiveNumber(item)
+              ? 'bg-gray-400 w-[1.125rem]'
+              : 'bg-gray-350 w-[0.375rem]')
+          }
+        />
+      ))}
     </div>
   )
 }
