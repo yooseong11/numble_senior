4bf1d09a84f3691888b077fd20efac57b1292eca
LeeYuseong <82145837+yuseong11@users.noreply.github.com>
2022-11-01 16:41:40 +0900

    fix: 스플래시 클릭시 다음 페이지로 넘어가도록

--- src/routes/splash.tsx
+++ src/routes/splash.tsx
@@ -13,7 +13,7 @@ const splash = () => {
   }, [])
 
   return (
-    <div className='flex flex-col items-center font-MICEGothc py-20'>
+    <div className='flex flex-col items-center font-MICEGothc py-20' onClick={goToOnboardingPage}>
       <div className='text-center'>
         <p className='text-xl'>
           당신의 <span className='text-yellow-500 font-bold'>인생 2막</span>을 응원하는
@@ -22,7 +22,7 @@ const splash = () => {
       </div>
       <img className='pt-14 w-60' src={splashImg} title='splashimg' />
       <h1>
-        <img className='w-44 pt-14 ' src={kbImg} title='kb국민은행' />
+        <img className='w-44 pt-14 animate-pulse' src={kbImg} title='kb국민은행' />
       </h1>
     </div>
   )
