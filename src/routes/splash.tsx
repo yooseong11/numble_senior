d62d9fca15240c934344da9128e987ca43f8e302
LeeYuseong <82145837+yuseong11@users.noreply.github.com>
2022-11-05 00:03:12 +0900

    pref: 이미지 최적화

--- src/routes/splash.tsx
+++ src/routes/splash.tsx
@@ -1,19 +1,19 @@
 import splashImg from '../image/splash.webp'
 import kbImg from '../image/kb.webp'
-import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const splash = () => {
   const navigate = useNavigate()
+
   const goToOnboardingPage = () => {
     navigate('/onboarding/1')
   }
-  useEffect(() => {
-    setTimeout(goToOnboardingPage, 3000)
-  }, [])
 
   return (
-    <div className='flex flex-col items-center font-MICEGothc py-20' onClick={goToOnboardingPage}>
+    <div
+      className='flex flex-col items-center font-MICEGothc py-20 px-16'
+      onClick={goToOnboardingPage}
+    >
       <div className='text-center'>
         <p className='text-xl'>
           당신의 <span className='text-yellow-500 font-bold'>인생 2막</span>을 응원하는
