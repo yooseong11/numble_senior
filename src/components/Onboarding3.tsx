b96f3f9da913d283175cda014a97112657fe005a
LeeYuseong <82145837+yuseong11@users.noreply.github.com>
2022-11-05 00:14:37 +0900

    fix: safari 문제 해결

--- src/components/Onboarding3.tsx
+++ src/components/Onboarding3.tsx
@@ -3,12 +3,24 @@ import slideUpImg from '../image/onboarding_3_2.webp'
 import Image from './Image'
 
 const Onboarding3 = () => {
-	return (
+  return (
     <div className='relative'>
-      <Image width={290} height={500}  src={CellPhoneImage} alt='img' className='m-auto animate-darker'  />
-      <Image width={235} height={100}  src={slideUpImg} alt='img' className='absolute bottom-8 ml-7 animate-slideUp' />
+      <Image
+        width={290}
+        height={500}
+        src={CellPhoneImage}
+        alt='img'
+        className='m-auto animate-darker'
+      />
+      <Image
+        width={235}
+        height={100}
+        src={slideUpImg}
+        alt='img'
+        className='absolute bottom-12 ml-7 animate-slideUp'
+      />
     </div>
-	)
+  )
 }
 
 export default Onboarding3
