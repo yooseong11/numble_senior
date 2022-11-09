f286dd340a752d1198f653d76a979faa1d4cda1c
LeeYuseong <82145837+yuseong11@users.noreply.github.com>
2022-11-09 17:05:55 +0900

    refactor: 배열 구조 분해 할당

--- src/components/OnboardingImageSection.tsx
+++ src/components/OnboardingImageSection.tsx
@@ -1,13 +1,16 @@
 import {OnboadringPageProps} from '@/onboardingDummyData'
 
 
-const OnboardingImageSection = ({ title, content,animate }:OnboadringPageProps) => {
+const OnboardingImageSection = ({ title, content, animate }: OnboadringPageProps) => {
+	
+	const [regularText, boldText] = title
+
 	return (
 		<>
 		<div className='text-center font-MICEGothc'>
 			<h2 className='text-3xl pb-2'>
-				{title[0]}{' '}
-				<span className='text-yellow-500 font-bold'>{title[1]}</span>
+				{regularText}{' '}
+				<span className='text-yellow-500 font-bold'>{boldText}</span>
 			</h2>
 			<p className='text-gray-500 pb-10'>{content}</p>
 		</div>
