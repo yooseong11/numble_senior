e317c5d0b13e9f3b07ac21f9f75d6176a18f726b
LeeYuseong <82145837+yuseong11@users.noreply.github.com>
2022-11-29 18:01:44 +0900

    fix: 컴포넌트 중복되는 로직 유틸 함수로 변경

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -1,16 +1,23 @@
+import classJoin from '@/utils/classJoin'
+
 interface NextButtonProps {
-	active?: boolean
-	onClick?: () => void
+  active?: boolean
+  onClick?: () => void
 }
 
-const NextButton = ({  active = false, onClick }: NextButtonProps) => {
-	
-	const activeColor = active ? 'bg-yellow-300' : 'bg-gray-300'
-	
-	return (
-		// eslint-disable-next-line @typescript-eslint/no-empty-function
-		<button onClick={active ? onClick : () => {}} className={`nextButton ${activeColor} text-gray-800 font-bold pt-3 pb-6 w-full font-NotoSans absolute bottom-0 hover:bg-yellow-500 rounded-b-lg`}>다음</button>
-	)	
+const NextButton = ({ active = false, onClick }: NextButtonProps) => {
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    <button
+      onClick={active ? onClick : () => {}}
+      className={classJoin(
+        'nextButton text-gray-800 font-bold pt-3 pb-6 w-full font-NotoSans absolute bottom-0 hover:bg-yellow-500 rounded-b-lg',
+        active ? 'bg-yellow-300' : 'bg-gray-300',
+      )}
+    >
+      다음
+    </button>
+  )
 }
 
 export default NextButton
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,3 +1,4 @@
+import classJoin from '@/utils/classJoin'
 interface PaginationProps {
   size: number
   activeNumber?: number
@@ -6,18 +7,13 @@ interface PaginationProps {
 const Pagination = ({ size, activeNumber }: PaginationProps) => {
   const pages = Array.from({ length: size }, (v, i) => i + 1)
 
-  const ActivePageStyle = (currentPage: number) => {
-    const isActiveNumber = currentPage === activeNumber
-    return isActiveNumber ? 'bg-gray-400 w-[1.125rem]' : 'bg-gray-350 w-[0.375rem]'
-  }
-
   return (
     <ul className='pagenation flex p-4'>
       {pages.map((currentPage) => (
         <li
           key={currentPage}
           className={
-            'rounded-full h-[0.375rem] m-[0.188rem] ' + (ActivePageStyle(currentPage))
+            classJoin('rounded-full h-[0.375rem] m-[0.188rem]', currentPage === activeNumber ? 'bg-gray-400 w-[1.125rem]' : 'bg-gray-350 w-[0.375rem]')
           }
         />
       ))}
