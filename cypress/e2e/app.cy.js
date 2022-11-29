510d09099632675197fa07f0a2f4aade93d3d54f
LeeYuseong <82145837+yuseong11@users.noreply.github.com>
2022-11-29 17:34:19 +0900

    test: 인덱스 페이지 테스트 설정

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,1 +1,15 @@
-/// <reference types="cypress" />
+
+describe('"/" index page', () => {
+	beforeEach(() => {
+		cy.visit('http://127.0.0.1:5173/')
+	})
+
+	it('첫 페이지가 나타난다.', () => {
+		cy.get('.title').should('have.text', '당신의 인생 2막을 응원하는평생 금융파트너')
+	})
+	
+	it('첫 화면을 클릭하면 다음 페이지로 넘어간다', () => {
+		cy.get('#screen').click();
+		cy.location('pathname').should('include', '/skipOrStart');
+	})
+})
--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -15,12 +15,12 @@ const Splash = () => {
       className='flex flex-col items-center font-MICEGothc py-10 px-16'
       onClick={goToOnboardingPage}
     >
-      <div className='text-center'>
+      <h2 className='text-center title'>
         <p className='text-xl'>
           당신의 <span className='text-yellow-500 font-bold'>인생 2막</span>을 응원하는
         </p>
         <p className='text-2xl'>평생 금융파트너</p>
-      </div>
+      </h2>
       <img className='pt-14 w-60' src={splashImg} title='kb국민은행' />
       <h1>
         <img className='w-44 pt-14 animate-pulse' src={kbImg} title='kb국민은행' />
